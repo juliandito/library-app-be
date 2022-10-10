78327d612bc989f6ea47504f0d7fd15fed49cf0d
juliandito <vjuliandito@gmail.com>
2022-10-10 17:28:49 +0700

    [UPDATE] postgresql settings | docker compose db

--- library_app_be/library_app_be/settings.py
+++ library_app_be/library_app_be/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'library_app_be.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "homestead",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
